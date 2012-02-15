d15db0ff5e1f57a6ba75a88e8e0aef433609297a
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-02-15 09:11:45 +0100

    DefyParts: change activity name to fit settings app
    Change-Id: I4c9592d98c9e407c6598d4510cd8cdb6f2dfa746

--- DefyParts/src/com/cyanogenmod/settings/device/DeviceSettings.java
+++ DefyParts/src/com/cyanogenmod/settings/device/DeviceSettings.java
@@ -17,7 +17,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class SettingsActivity extends PreferenceActivity implements OnPreferenceChangeListener {
+public class DeviceSettings extends PreferenceActivity implements OnPreferenceChangeListener {
     private static final String TAG = "DefyParts";
 
     private PreferenceCategory generalSettings;
