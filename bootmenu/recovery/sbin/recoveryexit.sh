e3f9e511cce7295f1e5d303e3cb0eb9386adcc43
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-03-04 10:15:28 +0100

    Fix recoveryexit.sh script

--- bootmenu/recovery/sbin/recoveryexit.sh
+++ bootmenu/recovery/sbin/recoveryexit.sh
@@ -1,4 +1,32 @@
-#!/system/bin/sh
+#!/sbin/busybox ash
+
+if [ ! -x /system/bin/bootmenu ]; then
+    mount /system
+fi
+
+if [ ! -f /cache/recovery/bootmode.conf ]; then
+    mount /cache
+fi
+
+if [ -f /system/bin/bootmenu ]; then
+
+    if [ -L /sbin/busybox ]; then
+        rm /sbin/busybox
+        # replace recovery symlink by full static busybox bootmenu version
+        /system/bootmenu/binary/busybox cp /system/bootmenu/binary/busybox /sbin/busybox
+
+        # keep recovery binary, for extra tools
+        # rm /sbin/recovery
+    fi
+
+    echo bootmenu > /cache/recovery/bootmode.conf
+    /system/bin/logwrapper /system/bin/bootmenu
+
+    # only for real recovery.img service
+    # /system/bin/toolbox stop recovery
+
+    exit 0
+fi
 
 # fast button warning
 echo 1 > /sys/class/leds/red/brightness
