e7a0837813192edcbc568670dfc3964b6d58d231
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-03-15 19:35:29 +0100

    Dont delete recursive /osh

--- bootmenu/script/2nd-init.sh
+++ bootmenu/script/2nd-init.sh
@@ -9,7 +9,7 @@ source /system/bootmenu/script/_config.sh
 mount -o remount,rw /
 rm -f /*.rc
 rm -f /*.sh
-rm -rf /osh
+rm -f /osh
 rm -rf /preinstall
 cp -f /system/bootmenu/2nd-init/* /
 ln -s /init /sbin/ueventd
