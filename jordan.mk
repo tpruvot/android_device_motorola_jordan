a81d910ea9651e2f513e363736885023d62c6aa3
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-04-06 08:41:56 +0200

    init.rc: remove vendor path references
    We dont have this path and this slow down (a little bit)
    all library load, made several times on each new process
    
    jordan.mk: Add lib/hw packages to restore optional tag
    
    Change-Id: I050a1ab8dbbdcac37458d9629a34cca4a3acd6cb

--- jordan.mk
+++ jordan.mk
@@ -100,7 +100,8 @@ PRODUCT_PACKAGES += libOMX.TI.VPP
 # Defy stuff
 PRODUCT_PACKAGES += libfnc DefyParts Usb 
 
-PRODUCT_PACKAGES += e2fsck
+# Core stuff
+PRODUCT_PACKAGES += charge_only_mode mot_boot_mode lights.jordan sensors.jordan e2fsck
 
 # Publish that we support the live wallpaper feature.
 PRODUCT_PACKAGES += LiveWallpapers LiveWallpapersPicker MagicSmokeWallpapers 
