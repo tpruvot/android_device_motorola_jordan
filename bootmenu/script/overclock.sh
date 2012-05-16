e40fc77cd77ead102f0a67a7b4cb1f9d518e2757
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-05-16 20:56:32 +0200

    overclock: more cleanup for cm patchset 6

--- bootmenu/script/overclock.sh
+++ bootmenu/script/overclock.sh
@@ -163,27 +163,16 @@ set_scaling()
       fi
 
       echo boosted > $SCALING_GOVERNOR
-      echo $bst_debug_mask
       echo $bst_debug_mask     > /sys/devices/system/cpu/cpufreq/boosted/debug_mask
-      echo $bst_awake_ideal_freq
       echo $bst_awake_ideal_freq > /sys/devices/system/cpu/cpufreq/boosted/awake_ideal_freq
-      echo $bst_down_rate_us
       echo $bst_down_rate_us   > /sys/devices/system/cpu/cpufreq/boosted/down_rate_us
-      echo $bst_max_cpu_load
       echo $bst_max_cpu_load   > /sys/devices/system/cpu/cpufreq/boosted/max_cpu_load
-      echo $bst_min_cpu_load
       echo $bst_min_cpu_load   > /sys/devices/system/cpu/cpufreq/boosted/min_cpu_load
-      echo $bst_ramp_down_step
       echo $bst_ramp_down_step > /sys/devices/system/cpu/cpufreq/boosted/ramp_down_step
-      echo $bst_ramp_up_step
       echo $bst_ramp_up_step   > /sys/devices/system/cpu/cpufreq/boosted/ramp_up_step
-      echo $bst_sample_rate_jiffies
       echo $bst_sample_rate_jiffies > /sys/devices/system/cpu/cpufreq/boosted/sample_rate_jiffies
-      echo $bst_sleep_ideal_freq
       echo $bst_sleep_ideal_freq    > /sys/devices/system/cpu/cpufreq/boosted/sleep_ideal_freq
-      echo $bst_sleep_wakeup_freq
       echo $bst_sleep_wakeup_freq   > /sys/devices/system/cpu/cpufreq/boosted/sleep_wakeup_freq
-      echo $bst_up_rate_us
       echo $bst_up_rate_us     > /sys/devices/system/cpu/cpufreq/boosted/up_rate_us
     ;;
     "6" )
