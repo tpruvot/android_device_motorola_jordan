50fe2c3bb732c18c3fde405b520172f4450cb0ed
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-06-16 22:31:58 +0200

    devmem2: add 64bit support and enhance mapping, tag it v2
    Change-Id: Id13bd6b872ceea0abcf65549b36c0ad41e60ee05

--- tools/devmem2.c
+++ tools/devmem2.c
@@ -1,12 +1,12 @@
 /*
  * devmem2.c: Simple program to read/write from/to any location in memory.
  *
- * v1.0.0 available as debian package
+ * v1.0 was available as debian package, also seen in busybox as devmem
  *
  *  Copyright (C) 2000, Jan-Derk Bakker (jdb@lartmaker.nl)
  *
- *  Warning fixes by Tanguy Pruvot for bionic (tpruvot@github)
- *
+ *  Warning fixes, volatile and 64bit r/w support
+ *     by Tanguy Pruvot for bionic (tpruvot@github)
  *
  * This software has been developed for the LART computing board
  * (http://www.lart.tudelft.nl/). The development has been sponsored by
@@ -46,23 +46,22 @@
 #define FATAL do { fprintf(stderr, "Error at line %d, file %s (%d) [%s]\n", \
   __LINE__, __FILE__, errno, strerror(errno)); exit(1); } while(0)
  
-#define MAP_SIZE 4096UL
-#define MAP_MASK (MAP_SIZE - 1)
-
 #define INCPTR(p,n) p = (void*) ( ((unsigned long) p) + ((unsigned long) n) )
 #define ADDPTR(p,n)     (void*) ( ((unsigned long) p) + ((unsigned long) n) )
 
 int main(int argc, char **argv) {
 	int fd;
 	void *map_base, *virt_addr;
-	unsigned long read_result, writeval;
+	uint64_t read_result = 0, writeval = 0;
 	off_t target;
 	int access_type = 'w';
+	unsigned page_size, mapped_size, offset_in_page;
 
 	if(argc < 2) {
-		fprintf(stderr, "\nUsage:\t%s { address } [ type [ data ] ]\n"
+		fprintf(stderr, "devmem2 v2.0 - tool to read/write from/to memory\n"
+			"Usage:\t%s { address } [ type [ data ] ]\n"
 			"\taddress : memory address to act upon\n"
-			"\ttype    : access operation type : [b]yte, [h]alfword, [w]ord\n"
+			"\ttype    : access operation type : [b]yte, [h]alf, d[w]ord, [l]ong(64)\n"
 			"\tdata    : data to be written\n\n",
 			argv[0]);
 		exit(1);
@@ -78,52 +77,71 @@ int main(int argc, char **argv) {
 	fflush(stdout);
 
 	/* Map one page */
-	map_base = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, target & ~MAP_MASK);
+	mapped_size = page_size = getpagesize();
+	offset_in_page = (unsigned)target & (page_size - 1);
+	if (offset_in_page + 8 > page_size) {
+		/* This access spans pages.
+		 * Must map two pages to make it possible
+		 */
+		mapped_size *= 2;
+	}
+
+	map_base = mmap(NULL, mapped_size,
+			PROT_READ | PROT_WRITE,
+			MAP_SHARED, fd,
+			target & ~(off_t)(page_size - 1));
+
 	if(map_base == (void *) -1) FATAL;
 	printf("Memory mapped at address %p.\n", map_base);
 	fflush(stdout);
 
-	//virt_addr = map_base + (target & MAP_MASK);
-	virt_addr = ADDPTR(map_base, (target & MAP_MASK));
+	virt_addr = ADDPTR(map_base, offset_in_page);
 
 	switch(access_type) {
 		case 'b':
-			read_result = *((unsigned char *) virt_addr);
+			read_result = *((volatile uint8_t*) virt_addr);
 			break;
 		case 'h':
-			read_result = *((unsigned short *) virt_addr);
+			read_result = *((volatile uint16_t*) virt_addr);
 			break;
 		case 'w':
-			read_result = *((unsigned long *) virt_addr);
+			read_result = *((volatile uint32_t*) virt_addr);
+			break;
+		case 'l':
+			read_result = *((volatile uint64_t*) virt_addr);
 			break;
 		default:
 			fprintf(stderr, "Illegal data type '%c'.\n", access_type);
 			exit(2);
 	}
-	printf("Value at address 0x%lX (%p): 0x%lX\n", target, virt_addr, read_result);
+	printf("Value at address 0x%lX (%p): 0x%llX\n", target, virt_addr, read_result);
 	fflush(stdout);
 
 	if(argc > 3) {
-		writeval = strtoul(argv[3], 0, 0);
+		writeval = strtoull(argv[3], 0, 0);
 		switch(access_type) {
 			case 'b':
-				*((unsigned char *) virt_addr) = writeval;
-				read_result = *((unsigned char *) virt_addr);
+				*((volatile uint8_t*) virt_addr) = writeval;
+				read_result = *((volatile uint8_t*) virt_addr);
 				break;
 			case 'h':
-				*((unsigned short *) virt_addr) = writeval;
-				read_result = *((unsigned short *) virt_addr);
+				*((volatile uint16_t*) virt_addr) = writeval;
+				read_result = *((volatile uint16_t*) virt_addr);
 				break;
 			case 'w':
-				*((unsigned long *) virt_addr) = writeval;
-				read_result = *((unsigned long *) virt_addr);
+				*((volatile uint32_t*) virt_addr) = writeval;
+				read_result = *((volatile uint32_t*) virt_addr);
+				break;
+			case 'l':
+				*((volatile uint64_t*) virt_addr) = writeval;
+				read_result = *((volatile uint64_t*) virt_addr);
 				break;
 		}
-		printf("Written 0x%lX; readback 0x%lX\n", writeval, read_result);
+		printf("Written 0x%llX; readback 0x%llX\n", writeval, read_result);
 		fflush(stdout);
 	}
 
-	if(munmap(map_base, MAP_SIZE) == -1) FATAL;
+	if(munmap(map_base, mapped_size) == -1) FATAL;
 
 	close(fd);
 	return 0;
