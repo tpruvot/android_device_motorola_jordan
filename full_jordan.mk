f01f78eb3a4f497f8c0ef773810353b12bbedb39
Quarx2k <agent00791@gmail.com>
2012-07-08 14:36:10 +0700

    wrong vendor

--- full_jordan.mk
+++ full_jordan.mk
@@ -23,7 +23,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/motorola/jordan/jordan.mk)
-$(call inherit-product, vendor/motorola/jordan_plus/jordan-vendor.mk)
+$(call inherit-product, vendor/motorola/jordan/jordan-vendor.mk)
 # recovery tools
 PRODUCT_COPY_FILES += \
 	out/target/product/jordan/recovery/root/sbin/tune2fs:system/bootmenu/recovery/sbin/tune2fs \
