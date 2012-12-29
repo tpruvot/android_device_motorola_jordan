f8d2d86ecb7d4b02990f3a72ed620d8026fd4d35
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-12-29 17:27:04 +0100

    Pimp BP panic handling. (ics variant)
    - Add automatic reboot including post-reboot notification dialog
    - Make user aware of BP panic not only by notification, but also by
      dialog (which also allows preventing or delaying the auto reboot)
    
    Change-Id: I6207355b42db53b591b2857ce556719460d7d9f7

--- prebuilt/bin/handle_bp_panic.sh
+++ prebuilt/bin/handle_bp_panic.sh
@@ -4,7 +4,7 @@
 setprop ctl.stop panic_daemon
 
 # let UI handle the failure
-am startservice -n com.cyanogenmod.defyparts/.BpPanicHandlerService 2>&1 | grep Error
+am startservice -a com.cyanogenmod.settings.device.action.HANDLE_BP_PANIC -n com.cyanogenmod.settings.device/.BpPanicHandlerService 2>&1 | grep Error
 if [ $? -eq 0 ]; then
     # some error occured, use fallback and reboot directly
     reboot bppanic
